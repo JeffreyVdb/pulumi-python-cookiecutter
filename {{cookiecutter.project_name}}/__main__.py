fbb71631e2b056e0e947fb1869b403a8b1ac7c44
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2023-10-10 17:07:49 +0200

    first commit

---
+++ {{cookiecutter.project_name}}/__main__.py
@@ -0,0 +1,5 @@
+"""A Python Pulumi program"""
+from {{cookiecutter.__project_name_no_dashes}}.main import create_resources
+
+
+create_resources()
---
+++ {{cookiecutter.project_name}}/{{cookiecutter.__project_name_no_dashes}}/main.py
@@ -0,0 +1,7 @@
+"""A Python Pulumi program"""
+
+import pulumi
+
+
+def create_resources():
+    pass
